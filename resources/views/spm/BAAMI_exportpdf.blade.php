8f4f8b29ce688d07651febf26f56f703cd16dc90
Alifan12 <alif129310@gmail.com>
2023-09-13 11:40:29 +0700

    ganti asset

--- resources/views/spm/BAAMI_exportpdf.blade.php
+++ resources/views/spm/BAAMI_exportpdf.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ public_path('bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ public_path('css/export.css') }}" media="print">
+    <link rel="stylesheet" href="{{ asset('css/export.css') }}" media="print">
     <title>Table</title>
     <style type="text/css">
         
