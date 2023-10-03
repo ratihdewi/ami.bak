13325367bbecc05437b5a48e43586ed79b51cb11
Alifan12 <alif129310@gmail.com>
2023-10-03 11:37:33 +0700

    perbaikan form pertanyaan auditee

--- resources/views/inc/head.blade.php
+++ resources/views/inc/head.blade.php
@@ -57,7 +57,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.10.0/css/bootstrap-datepicker.min.css" integrity="sha512-34s5cpvaNG3BknEWSuOncX28vz97bRI59UnVtEEpFX536A7BtZSJHsDyFoCl8S7Dt2TPzcrCEoHBGeM4SUBDBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 {{-- Jadwal Audit End --}}
-
+<link rel="stylesheet" href="/css/tinyAuditee.css" />
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
