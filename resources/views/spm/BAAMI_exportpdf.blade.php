c294d24a907bc4d31e19a954280ee11ea0f9f109
Alifan12 <alif129310@gmail.com>
2023-10-09 08:40:10 +0700

    test esport pdf

--- resources/views/spm/BAAMI_exportpdf.blade.php
+++ resources/views/spm/BAAMI_exportpdf.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ public_path('bootstrap.min.css') }}">
-    <link rel="stylesheet" media="all" href="{{ public_path('css/export.css') }}">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ public_path('bootstrap.min.css') }}"> --}}
+    <link rel="stylesheet" type="text/css" href="/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ public_path('css/export.css') }}">
     <title>Table</title>
-    <style type="text/css">
+    <style>
         
         body {
             font-family: 'Times New Roman', Times, serif;
